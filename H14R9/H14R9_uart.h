1385d1057bd0606fd1bc13b042f8a2f48e8197bb
ADEL\adool <adoolfaki@gmail.com>
2025-06-24 17:02:06 +0300

    add ae empty firmware

---
+++ H14R9/H14R9_uart.h
@@ -0,0 +1,32 @@
+/*
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
+ All rights reserved
+
+ File Name  : H14R9_uart.h
+ Description: Declares UART operation functions.
+ UART: Mutex-protected read/write (polling/interrupt), port direction messaging.
+ */
+
+/* Define to prevent recursive inclusion ***********************************/
+#ifndef __usart_H
+#define __usart_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ****************************************************************/
+#include "BOS.h"
+
+/* Exported Functions ******************************************************/
+extern HAL_StatusTypeDef readPxMutex(uint8_t port,char *buffer,uint16_t n,uint32_t mutexTimeout,uint32_t portTimeout);
+extern HAL_StatusTypeDef writePxMutex(uint8_t port,char *buffer,uint16_t n,uint32_t mutexTimeout,uint32_t portTimeout);
+extern HAL_StatusTypeDef readPxITMutex(uint8_t port,char *buffer,uint16_t n,uint32_t mutexTimeout);
+extern HAL_StatusTypeDef writePxITMutex(uint8_t port,char *buffer,uint16_t n,uint32_t mutexTimeout);
+extern BOS_Status ReadPortsDirMSG(uint8_t SourceModule);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ usart_H */
+
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
---
+++ User/main.c
@@ -0,0 +1,38 @@
+/*
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
+ All rights reserved
+
+ File Name     : main.c
+ Description   : Main program body.
+ */
+
+/* Includes ****************************************************************/
+#include "BOS.h"
+
+/* Private variables *******************************************************/
+
+/* Private Function Prototypes *********************************************/
+
+/* Main Function ***********************************************************/
+int main(void){
+
+	/* Initialize Module &  BitzOS */
+	Module_Init();
+
+	/* Don't place your code here */
+	for(;;){
+	}
+}
+
+/***************************************************************************/
+/* User Task */
+void UserTask(void *argument){
+
+	/* put your code here, to run repeatedly. */
+	while(1){
+
+	}
+}
+
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
