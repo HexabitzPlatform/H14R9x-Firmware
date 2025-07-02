52aec25bda7634fcb3175ce2cbef140a0ea79731
Ahmad Abdulhai <@Aa52585258aA@>
2025-07-02 13:00:30 +0300

    Editing DMA_PORT_INDEX_Px on uart HAL_UART_MspInit()

--- User/topology.h
+++ User/topology.h
@@ -24,14 +24,14 @@
 #define _mod2	2<<3
 
 // Topology
-static uint16_t array[__N ][7] ={
-      	{_H0BR4, _mod2 | P1, 0, 0, 0, 0, 0}, 								 // Module 1
+static uint16_t Array[__N ][7] ={
+      	{_H14R9, _mod2 | P1, 0, 0, 0, 0, 0}, 								 // Module 1
         {_H01R0, _mod1 | P1, 0, 0, 0, 0, 0},					    		 // Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H0BR4	1
+	#define	 H14R9	       1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
